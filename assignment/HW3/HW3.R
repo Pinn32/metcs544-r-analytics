7021ce51c7291fe81e4d069f69fb43044567664a
Pinn Xu <32sanliang32@gmail.com>
2026-01-30 23:32:42 -0500

    finish 2-a 2-b (2-b need inference)

--- assignment/HW3/HW3.R
+++ assignment/HW3/HW3.R
@@ -137,33 +137,100 @@ us_quarters <- read.csv("https://people.bu.edu/kalathur/datasets/us_quarters.csv
 head(us_quarters)
 
 
-# a) state with highest num of quarters by each mint --------------------------
+# a) state with min/max num of quarters by each mint --------------------------
 
+# find states with highest/lowest number by each mint
+(min_Denver <- us_quarters[which.min(us_quarters$DenverMint),"State"])
+(max_Denver <- us_quarters[which.max(us_quarters$DenverMint),"State"])
 
+(min_Philly <- us_quarters[which.min(us_quarters$PhillyMint),"State"])
+(max_Philly <- us_quarters[which.max(us_quarters$PhillyMint),"State"])
 
+# print out results
+paste(
+  sprintf(
+    "DenverMint produced most quarters for %s and least for %s.", 
+    max_Denver, min_Denver
+  ),
+  sprintf(
+    "PhillyMint produced most quarters for %s and least for %s.",
+    max_Philly, min_Philly
+  )
+)
 
 
 # b) barplot: matrix for the two mints ----------------------------------------
 
+# store the data into a matrix
+m <- as.matrix(us_quarters[-1]) # exclude the 1st column
 
+# find suitable y limit
+y_max_2b <- max(pretty(m))
 
+# adjust parameters
+  par(
+      mai=c(2, 1, 1, 0.5),  # margin in inches
+      cex=0.8  # all text size become 80% of original
+    )
+
+# asjust global option to avoid scientific notation
+options(scipen = 999)
+
+# barplot
+barplot(
+  t(m),  # transpose matrix
+  beside = TRUE,  # let bar groups beside each other
+  ylim = c(0,y_max_2b),  # y limits
+  names.arg = us_quarters$State,  # category names
+  las = 2,  # let labels perpendicular to axis
+  col = c('blue', 'grey'),  # colors
+
+  legend.text = T,  # show legend
+    args.legend = list(
+      text.width = 17,  # legend width
+      x = "topright",  # legend position
+      x.intersp = 1,  # inter space
+      y.intersp = 0.7,  # line space
+      cex = 0.8  # text size becomes 80% of original
+    )
+)
 
 
 # two striking inferences
 
+# According to Figure 4, the PhillyMint shows a dominant production volume 
+# for state Virginia, reaching a billion units, which is far more than 
+# that of DenverMint in Virginia (less than 700 million). This is also 
+# the highest peak in the entire dataset.
 
-
-
+# In addition, Connecticut is the state for which the largest 
+# production volume by DenverMint was (about 700 million).
 
 
 
 # c) side-by-side box plots for the two mints ---------------------------------
 
+# set parameters
+par(
+  mar=c(4, 8, 4, 6), cex=0.8,  # margin and general text size
+  mgp = c(5, 1, 0)  # move y axis title left to avoid overlap with y ticks
+)
 
+# Drawing the side-by-side boxplot
+boxplot(
+  us_quarters$DenverMint, us_quarters$PhillyMint,
+  col = c("blue", "grey"),  # colors
+  las = 1,  # keep y labels horizontal
+  outline = TRUE,  # show outliers if exist
+  names = c("Denver Mint", "Philly Mint"),  # category names
+  ylab = "Number of Quarters",  # y label
+  main = "Comparison of Quarters Produced by Mint"  # title
+)
 
 
 # two striking inferences for each of the box plots
 
+# 🔴从这继续
 
 
 
@@ -177,6 +244,9 @@ head(us_quarters)
 
 
 
+# reset global option & par
+options(scipen = 0)
+par(cex = 1)
 
 
 
